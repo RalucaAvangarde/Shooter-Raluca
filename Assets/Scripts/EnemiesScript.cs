71c55bcff0f8e990c3c1d8afa39692197b9d19f4
RalucaAvangarde <raluca.metes@avangarde-software.com>
2019-07-01 15:14:01 +0300

    room

---
+++ Assets/Scripts/EnemiesScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy;
+    [SerializeField]
+    private Transform container;
+   
+
+    public void InstatiateEnemy(int nr)
+    {
+        var position = new Vector3(Random.Range(-15, 5), 0 , Random.Range(-15, 20));
+        for (int i = 0; i < nr; i++)
+        {
+            Instantiate(enemy, position, Quaternion.identity,container); 
+        }
+    }
+}
--- Assets/Scripts/ShooterScript.cs
+++ Assets/Scripts/ShooterScript.cs
@@ -6,24 +6,26 @@ public class ShooterScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject gunPoint;
+    public Camera cam;
     [SerializeField]
     private GameObject enemie;
-    // Start is called before the first frame update
+    
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(gunPoint.transform.position, gunPoint.transform.forward, out hit, 100))
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 1000))
         {
-            if (hit.collider.gameObject.tag == "inamic")
+            if (hit.collider.gameObject.tag == "enemy")
             {
+                Debug.DrawRay(this.transform.position, this.transform.forward, Color.green);
                 Debug.Log("is here");
-                enemie.SetActive(false);
+               // enemie.SetActive(false);
+
             }
         }
     }
