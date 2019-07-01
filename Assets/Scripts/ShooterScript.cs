4177f94cc56c846875dc64e55bfef273d1451a8d
RalucaAvangarde <raluca.metes@avangarde-software.com>
2019-07-01 11:27:31 +0300

    Xr settings

---
+++ Assets/Scripts/ShooterScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gunPoint;
+    [SerializeField]
+    private GameObject enemie;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(gunPoint.transform.position, gunPoint.transform.forward, out hit, 100))
+        {
+            if (hit.collider.gameObject.tag == "inamic")
+            {
+                Debug.Log("is here");
+                enemie.SetActive(false);
+            }
+        }
+    }
+}
